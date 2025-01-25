acafb9b879596bdd23b7a20d0229884a120a5fae
BOSS1480 <yboss1480@gmail.com>
2025-01-26 00:32:47 +0200

    Update config.py

--- config.py
+++ config.py
@@ -9,9 +9,6 @@ class Config:
     DATABASE_NAME = environ.get("DATABASE_NAME", "vj-forward-bot")
     BOT_OWNER = int(environ.get("BOT_OWNER", "7036460933"))
 
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
 
 class temp(object): 
     lock = {}
@@ -19,7 +16,3 @@ class temp(object):
     forwardings = 0
     BANNED_USERS = []
     IS_FRWD_CHAT = []
-
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
