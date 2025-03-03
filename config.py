b8d25d872d8d004646b8477517a2741de7752245
BOSS1480 <yboss1480@gmail.com>
2025-03-03 02:05:21 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 from os import environ 
 
 class Config:
-    API_ID = int(environ.get("API_ID", "22558238"))
-    API_HASH = environ.get("API_HASH", "41abc14dd9f760887a50f9cd2cc1bb73")
+    API_ID = int(environ.get("API_ID", ""))
+    API_HASH = environ.get("API_HASH", "")
     BOT_TOKEN = environ.get("BOT_TOKEN", "") 
     BOT_SESSION = environ.get("BOT_SESSION", "vjbot") 
-    DATABASE_URI = environ.get("DATABASE_URI", "mongodb+srv://y0534150646:330833039@cluster0.ysd9p.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    DATABASE_URI = environ.get("DATABASE_URI", "mongodb+srv://name:12345678@cluster0.ysd9p.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     DATABASE_NAME = environ.get("DATABASE_NAME", "vj-forward-bot")
-    BOT_OWNER = int(environ.get("BOT_OWNER", "6335855540"))
+    BOT_OWNER = int(environ.get("BOT_OWNER", ""))
 
 
 class temp(object): 
